1e551f0b6f9954c3c6f1c0bc0b3c477afc03ea14
aleezacabahug <143432334+aleezacabahug@users.noreply.github.com>
2023-09-18 12:26:12 -0400

    Understand solutions for Project 2

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,19 +1,22 @@
-function MakeMultiFilter(originalArray) {
-    let currentArray = originalArray.slice();
-  
-    function arrayFilterer(filterCriteria, callback) {
-      if (typeof filterCriteria !== 'function') {
-        return currentArray;
-      }
-  
-      currentArray = currentArray.filter(filterCriteria);
-  
-      if (typeof callback === 'function') {
-        callback.call(originalArray, currentArray);
-      }
+'use strict';
 
-      return arrayFilterer;
-    }
-
+function MakeMultiFilter(originalArray){
+    const arrayFilterer = function (filterCriteria = undefined, callback = undefined){
+        if (arrayFilterer.currentArray === undefined){
+            arrayFilterer.currentArray = originalArray;
+        }
+        if (Array.isArray(originalArray)) {
+            if ((typeof filterCriteria === "function")) {
+                arrayFilterer.currentArray =
+                    arrayFilterer.currentArray.filter(e => filterCriteria(e));
+            } else {
+                return arrayFilterer.currentArray;
+            }
+        }
+        if (typeof callback === 'function'){
+            callback.call(originalArray, arrayFilterer.currentArray);
+        }
+        return arrayFilterer;
+    };
     return arrayFilterer;
-  }
+}
--- template-processor.js
+++ template-processor.js
@@ -1,30 +1,18 @@
-function TemplateProcessor(template) {
-    this.template = template;
-  }
+'use strict';
 
-  TemplateProcessor.prototype.fillIn = function (dictionary) {
-    var space = /{{(.*?)}}/g;
-
-    var filledTemplate = this.template.replace(space, function (same, property) {
-      return dictionary[property] !== undefined ? dictionary[property] : '';
-    });
-
-    return filledTemplate;
-  };
-
-
-
-
-
-var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
-var dateTemplate = new TemplateProcessor(template);
-
-var dictionary = {month: 'July', day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary);
-
-assert(str === 'My favorite month is July but not the day 1 or the year 2016');
-
-var dictionary2 = {day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary2);
-
-assert(str === 'My favorite month is  but not the day 1 or the year 2016');
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
+            }
+        }
+        const regex = /{{.*}}/g;
+        returnString = returnString.replaceAll(regex,"");
+        return returnString;
+    }
+}
